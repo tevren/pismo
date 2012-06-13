b4eb899b0226c7f74e158ddd8ce1856b19fb3e13
Anurag Mohanty <anurag@columbia.edu>
2012-06-13 13:19:49 -0400

    trying to fix merge

--- lib/pismo/document.rb
+++ lib/pismo/document.rb
@@ -8,8 +8,11 @@ module Pismo
   class Document
 
     attr_reader :doc, :url, :options
-
     ATTRIBUTE_METHODS = InternalAttributes.instance_methods + ExternalAttributes.instance_methods
+
+    include Pismo::InternalAttributes
+    include Pismo::ExternalAttributes
+
     DEFAULT_OPTIONS = {
       :image_extractor => true,
       :min_image_width => 100,
@@ -29,7 +32,6 @@ module Pismo
     def load(handle, url = nil)
       @url = url if url
       @url = handle if handle =~ /\Ahttp/i
-      @is_homepage = URI.parse(handle).request_uri == "/" if handle
       @html = if handle =~ /\Ahttp/i
                 open(handle).read
               elsif handle.is_a?(StringIO) || handle.is_a?(IO) || handle.is_a?(Tempfile)
@@ -37,30 +39,11 @@ module Pismo
               else
                 handle
               end
-      @html = self.class.clean_html(@html)
       @doc = Nokogiri::HTML(@html)
     end
 
     def match(args = [], all = false)
       @doc.match([*args], all)
-    end
-    def self.clean_html(html)
-      # Normalize stupid entities
-      # TODO: Optimize this so we don't need all these sequential gsubs
-      html.gsub!("&#8194;", " ")
-      html.gsub!("&#8195;", " ")
-      html.gsub!("&#8201;", " ")
-      html.gsub!('&#8211;', '-')
-      html.gsub!("&#8216;", "'")
-      html.gsub!('&#8217;', "'")
-      html.gsub!('&#8220;', '"')
-      html.gsub!('&#8221;', '"')
-      html.gsub!("&#8230;", '...')
-      html.gsub!('&nbsp;', ' ')
-      html.gsub!('&lt;', '<')
-      html.gsub!('&gt;', '>')
-      html.gsub!('&amp;', '&')
-      html
     end
   end
 end
